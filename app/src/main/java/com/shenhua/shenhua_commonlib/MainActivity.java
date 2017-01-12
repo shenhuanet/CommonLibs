ecccf48d9411db2a37096ffd1986acc6f97c43e9
shenhuanet <shenhuanet@126.com>
2017-01-12 19:25:50 +0800

    modify.

--- app/src/main/java/com/shenhua/shenhua_commonlib/MainActivity.java
+++ app/src/main/java/com/shenhua/shenhua_commonlib/MainActivity.java
@@ -1,13 +1,27 @@
 package com.shenhua.shenhua_commonlib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initFitsWindow();
         setContentView(R.layout.activity_main);
+    }
+
+    private void initFitsWindow() {
+//        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Window window = getWindow();
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+//        }
     }
 }
